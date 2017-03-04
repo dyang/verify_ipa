5e68fe56b4397300a31426b955534e52b363745d
Derek Yang <yanghada@gmail.com>
2017-03-04 21:32:36 +0100

    Deleted default action.

--- spec/verify_ipa_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::VerifyIpaAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The verify_ipa plugin is working!")
-
-      Fastlane::Actions::VerifyIpaAction.run(nil)
-    end
-  end
-end
