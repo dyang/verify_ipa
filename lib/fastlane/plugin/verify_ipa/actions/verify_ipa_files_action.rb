f329a7098dc1fb23eea30b071b2d73401c72c7ff
Derek Yang <yanghada@gmail.com>
2017-03-04 21:32:16 +0100

    Extracted app_path to helper in order to support more actions with similar need.

--- lib/fastlane/plugin/verify_ipa/actions/verify_ipa_files_action.rb
+++ lib/fastlane/plugin/verify_ipa/actions/verify_ipa_files_action.rb
@@ -3,19 +3,9 @@ module Fastlane
     class VerifyIpaFilesAction < Action
       def self.run(params)
         Dir.mktmpdir do |dir|
-          app_path = self.app_path(params, dir)
+          app_path = Helper::VerifyIpaHelper.app_path(params, dir)
           self.verify_files(params, app_path)
         end
-      end
-
-      def self.app_path(params, dir)
-        ipa_path = params[:ipa_path] || Actions.lane_context[SharedValues::IPA_OUTPUT_PATH] || ''
-        UI.user_error!("Unable to find ipa file '#{ipa_path}'.") unless File.exist?(ipa_path)
-
-        ipa_path = File.expand_path(ipa_path)
-        `unzip '#{ipa_path}' -d #{dir}`
-        UI.user_error!("Unable to unzip ipa '#{ipa_path}'") unless $? == 0
-        File.expand_path("#{dir}/Payload/*.app")
       end
 
       def self.verify_files(params, app_path)
--- lib/fastlane/plugin/verify_ipa/helper/verify_ipa_helper.rb
+++ lib/fastlane/plugin/verify_ipa/helper/verify_ipa_helper.rb
@@ -1,11 +1,14 @@
 module Fastlane
   module Helper
     class VerifyIpaHelper
-      # class methods that you define here become available in your action
-      # as `Helper::VerifyIpaHelper.your_method`
-      #
-      def self.show_message
-        UI.message("Hello from the verify_ipa plugin helper!")
+      def self.app_path(params, dir)
+        ipa_path = params[:ipa_path] || Actions.lane_context[SharedValues::IPA_OUTPUT_PATH] || ''
+        UI.user_error!("Unable to find ipa file '#{ipa_path}'.") unless File.exist?(ipa_path)
+
+        ipa_path = File.expand_path(ipa_path)
+        `unzip '#{ipa_path}' -d #{dir}`
+        UI.user_error!("Unable to unzip ipa '#{ipa_path}'") unless $? == 0
+        File.expand_path("#{dir}/Payload/*.app")
       end
     end
   end
